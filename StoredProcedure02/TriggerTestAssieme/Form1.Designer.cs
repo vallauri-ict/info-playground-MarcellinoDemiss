82f19461acf969f14585fe9eb0d306fff8536741
MarcellinoDemiss <61838576+MarcellinoDemiss@users.noreply.github.com>
2021-02-24 17:32:36 +0100

    add es storedProcedure

---
+++ StoredProcedure02/TriggerTestAssieme/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace TriggerTestAssieme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAggiornamenti = new System.Windows.Forms.DataGridView();
+            this.dgvCancellazioni = new System.Windows.Forms.DataGridView();
+            this.btnFunction1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAggiornamenti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCancellazioni)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAggiornamenti
+            // 
+            this.dgvAggiornamenti.AllowUserToAddRows = false;
+            this.dgvAggiornamenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAggiornamenti.Location = new System.Drawing.Point(12, 13);
+            this.dgvAggiornamenti.Name = "dgvAggiornamenti";
+            this.dgvAggiornamenti.Size = new System.Drawing.Size(323, 196);
+            this.dgvAggiornamenti.TabIndex = 0;
+            // 
+            // dgvCancellazioni
+            // 
+            this.dgvCancellazioni.AllowUserToAddRows = false;
+            this.dgvCancellazioni.AllowUserToDeleteRows = false;
+            this.dgvCancellazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCancellazioni.Location = new System.Drawing.Point(12, 242);
+            this.dgvCancellazioni.Name = "dgvCancellazioni";
+            this.dgvCancellazioni.RowHeadersVisible = false;
+            this.dgvCancellazioni.Size = new System.Drawing.Size(323, 196);
+            this.dgvCancellazioni.TabIndex = 2;
+            // 
+            // btnFunction1
+            // 
+            this.btnFunction1.Location = new System.Drawing.Point(363, 13);
+            this.btnFunction1.Name = "btnFunction1";
+            this.btnFunction1.Size = new System.Drawing.Size(159, 23);
+            this.btnFunction1.TabIndex = 3;
+            this.btnFunction1.Text = "Carica";
+            this.btnFunction1.UseVisualStyleBackColor = true;
+            this.btnFunction1.Click += new System.EventHandler(this.btnAggDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 450);
+            this.Controls.Add(this.btnFunction1);
+            this.Controls.Add(this.dgvCancellazioni);
+            this.Controls.Add(this.dgvAggiornamenti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAggiornamenti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCancellazioni)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAggiornamenti;
+        private System.Windows.Forms.DataGridView dgvCancellazioni;
+        private System.Windows.Forms.Button btnFunction1;
+    }
+}
+
---
+++ StoredProcedure02/TriggerTestAssieme/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace TriggerTestAssieme
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string workingDirectory = Environment.CurrentDirectory;
+        public static readonly string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        public static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + projectDirectory + @"\DriverDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+        private void btnAggDB_Click(object sender, EventArgs e)
+        {
+            PopulateDgv(0);
+            PopulateDgv(1);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PopulateDgv(int tables)
+        {
+            switch (tables)
+            {
+                case 0:
+                    SqlConnection con = new SqlConnection(CONNECTION_STRING);
+                    con.Open();
+                    string sql = "CercaAggiornamentiData";
+
+                    SqlCommand cmd = new SqlCommand(sql, con); 
+                    SqlParameter data = new SqlParameter();
+                    data.ParameterName = "data";
+                    data.Direction = ParameterDirection.Input;
+                    data.DbType = DbType.DateTime;
+                    data.Value = "01/01/2000";
+                    cmd.Parameters.Add(data);
+
+                    cmd.CommandText = "CercaAggiornamentiData";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            dgvAggiornamenti.Rows.Add(reader.GetValue(0));
+                        }
+                    }
+                    break;
+                case 1:
+                    con = new SqlConnection(CONNECTION_STRING);
+                    con.Open();
+                    string sqla = "CercaCancellazioniData";
+                    con.Open();
+
+                    cmd = new SqlCommand(sqla, con);
+                    data = new SqlParameter();
+                    data.ParameterName = "data";
+                    data.Direction = ParameterDirection.Input;
+                    data.DbType = DbType.DateTime;
+                    data.Value = "01/01/2000";
+                    cmd.Parameters.Add(data);
+
+                    cmd.CommandText = "CercaAggiornamentiData";
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            dgvAggiornamenti.Rows.Add(reader.GetValue(0));
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
